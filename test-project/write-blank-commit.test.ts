294fd234004f57bf62aeffb2d47ebb75a17136cd
Ben Limmer <ben@benlimmer.com>
2021-01-18 18:13:53 -0700

    try this

---
+++ test-project/write-blank-commit.test.ts
@@ -0,0 +1,13 @@
+import { spawnSync } from "child_process";
+
+if (process.env.CIRCLECI) {
+  describe("circleci", () => {
+    it("writes a test commit to avoid writing a cache", () => {
+      spawnSync("echo", ["change", ">>", "README.md"], {
+        shell: true,
+      });
+      spawnSync("git", ["add", "README.md"]);
+      spawnSync("git", ["commit", "-m", "no-op commit"]);
+    });
+  });
+}
